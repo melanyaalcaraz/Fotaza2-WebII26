834863a722cf308c7537f03f550053d3974caadc
Melany Alcaraz <103854523+melanyaalcaraz@users.noreply.github.com>
2026-05-27 20:04:20 -0300

    cambiando conexion
    para render

--- base_datos/conexion.js
+++ base_datos/conexion.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'fotaza'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 conexion.connect((err) => {
